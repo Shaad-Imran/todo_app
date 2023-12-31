8db1e17afd316f96bf2d6e0d2fdc27782b6c6e40
Imran <imranmansuri038@gmail.com>
2023-12-31 17:32:24 +0530

    main.jsx file updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx";
 import { Provider } from "react-redux";
 import appStore from "./store";
 import "./styles/GlobalStyles.css";
@@ -8,14 +8,8 @@ import "@fontsource/poppins/500.css";
 import "@fontsource/poppins/600.css";
 import "@fontsource/poppins/700.css";
 
-// Render the application into the root element
-const rootElement = document.getElementById("root");
-const app = (
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={appStore}>
     <App />
   </Provider>
 );
-
-// Create a root for concurrent mode and render the app
-const root = ReactDOM.createRoot(rootElement);
-root.render(app);
